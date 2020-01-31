d336cb47ba41bb18586a4e9a575b8d0dd60a70da
Paulo Jr <javadevzn@gmail.com>
2020-01-30 23:35:51 -0300

    Initial Files and Understanding the Vue JS Framework

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Sistema de Cotas do Mercado Financeiro"/>
+    
+    <TodoTasks :todoTasks="tasksList" />
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TodoTasks from './components/TodoTasks.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    TodoTasks
+  }, 
+  data: function(){
+    return {
+      tasksList : [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoTasks.vue
@@ -0,0 +1,20 @@
+<template>
+  <ol>
+    <li class="left-align"
+      v-for="item in todoTasks"
+      :key="item.id"> {{item.text}} </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: 'TodoTasks',
+  props: {todoTasks: Object}
+}
+</script>
+
+<style>
+.left-align {
+  text-align: left;
+}
+</style>
