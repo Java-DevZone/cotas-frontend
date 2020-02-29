173afbc5c507d1f275253ad99d42e1bf67d49c71
Lucas Leandro <lucasleandro380@gmail.com>
2020-02-29 17:27:10 -0300

    :sparkles: dashboard

--- src/pages/dashboard/Dashboard.vue
+++ src/pages/dashboard/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="flex">
-      <nav class="sidenav bg-indigo-900 font-bold pl-4 h-screen">
+      <nav class="sidenav hidden sm:flex bg-indigo-900 font-bold pl-4 h-screen">
         <ul class="flex flex-col p-4 text-gray-300">
           <li class="my-2"><span class="mr-3">1</span>Painel</li>
           <li class="my-2"><span class="mr-3">2</span>Enviar</li>
@@ -17,28 +17,28 @@
           <span>Este é o sumário das suas carteiras</span>
         </div>
         <div class="inline-flex justify-between w-full mt-3">
-          <div class="flex justify-between w-full w-2/4">
+          <div class="flex w-full flex-wrap">
             <div class="flex text-white flex-wrap">
               <div
-                class="m-3 flex flex-col justify-center shadow-md rounded bg-blue-500 mx-2 p-3"
+                class="m-3 flex flex-col justify-center shadow-md rounded bg-blue-500 mx-2 p-3 h-32"
               >
                 <span>RUPX Balance</span>
                 <h2>2,245.85 <sub>RUPX</sub></h2>
               </div>
               <div
-                class="m-3 flex flex-col justify-center shadow-md rounded bg-blue-500 mx-2 p-3"
+                class="m-3 flex flex-col justify-center shadow-md rounded bg-blue-500 mx-2 p-3 h-32"
               >
                 <span>RUPX Balance</span>
                 <h2>2,245.85 <sub>RUPX</sub></h2>
               </div>
               <div
-                class="m-3 flex flex-col justify-center shadow-md rounded bg-blue-500 mx-2 p-3"
+                class="m-3 flex flex-col justify-center shadow-md rounded bg-blue-500 mx-2 p-3 h-32"
               >
                 <span>RUPX Balance</span>
                 <h2>2,245.85 <sub>RUPX</sub></h2>
               </div>
             </div>
-            <div class="flex w-1/2 bg-white shadow-md">
+            <div class="flex w-full bg-white shadow-md">
               <div class="w-full p-3">
                 <span class="font-bold">Transações recentes</span>
                 <hr />
