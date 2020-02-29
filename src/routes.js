d5c4b7a3f5372595b4790a19797476a9a7add81b
Lucas Leandro <lucasleandro380@gmail.com>
2020-02-29 18:15:03 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,10 @@ import Wallet from "./pages/wallet/Wallet";
 import Dashbaord from "./pages/dashboard/Dashboard";
 import Investments from './pages/investments/Investment';
 import Profile from './pages/profile/Profile';
-
-export const routes = [
-  { path: "/wallet", component: Wallet },
   
 export const routes = [
   {path: '/wallet', component: Wallet},
   {path: '/investments', component: Investments},
-  { path: "/dashboard", component: Dashbaord },
+  {path: "/dashboard", component: Dashbaord },
   {path: '/profile', component: Profile},
 ];
