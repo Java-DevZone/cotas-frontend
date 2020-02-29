866b5c28c0f236fff5574ca81e4234774fcc5c7d
Goufix <alifer.17.11@gmail.com>
2020-02-29 15:53:52 -0300

    Add route for investments component

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import Wallet from './pages/wallet/Wallet';
+import Investments from './pages/investments/Investment';
 
 export const routes = [
   {path: '/wallet', component: Wallet},
+  {path: '/investments', component: Investments},
 ];
